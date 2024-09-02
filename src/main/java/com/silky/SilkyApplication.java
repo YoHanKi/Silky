c47c8cfeba36502140f3f30da67a2c4eb45bea8f
기묘한 주니어 개발자 <alcuz137@gmail.com>
2024-09-02 12:05:13 +0900

    silky initializr add

---
+++ src/main/java/com/silky/SilkyApplication.java
@@ -0,0 +1,13 @@
+package com.silky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SilkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/silky/SilkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilkyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
